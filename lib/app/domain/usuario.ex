0f17df73383c48f1e1dde240c375f8e76d756900
miguel londono <miguela.londonog1@uqvirtual.edu.co>
2025-11-18 01:04:36 -0500

    Agregar comentarios descriptivos al módulo Domain.Usuario

--- lib/app/domain/usuario.ex
+++ lib/app/domain/usuario.ex
@@ -1,6 +1,8 @@
 defmodule HackathonApp.Domain.Usuario do
+  # Estructura que representa un usuario dentro del sistema
   defstruct [:id, :nombre, :rol]
 
+  # Constructor para crear un nuevo usuario
   def nuevo(id, nombre, rol) do
     %__MODULE__{
       id: id,
