d99147d0d3372b86a34a56bb3c0caed1ec21a780
miguel londono <miguela.londonog1@uqvirtual.edu.co>
2025-11-18 01:03:05 -0500

    Agregar comentarios descriptivos al módulo Domain.Mensaje

--- lib/app/domain/mensaje.ex
+++ lib/app/domain/mensaje.ex
@@ -1,8 +1,10 @@
 defmodule HackathonApp.Domain.Mensaje do
   @moduledoc "Entidad Mensaje: representa un mensaje en el chat de un equipo"
 
+  # Estructura del mensaje con sus campos
   defstruct [:id, :equipo_id, :usuario_nombre, :contenido, :fecha]
 
+  # Constructor para crear un nuevo mensaje
   def nuevo(id, equipo_id, usuario_nombre, contenido, fecha) do
     %__MODULE__{
       id: id,
