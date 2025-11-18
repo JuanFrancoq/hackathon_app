a5591003065176eca4eac5c080af67857b009226
miguel londono <miguela.londonog1@uqvirtual.edu.co>
2025-11-18 01:02:01 -0500

    Añadir documentación básica al módulo Equipo

--- lib/app/domain/equipo.ex
+++ lib/app/domain/equipo.ex
@@ -1,6 +1,13 @@
 defmodule HackathonApp.Domain.Equipo do
+  @moduledoc """
+  Estructura básica que representa un equipo dentro del sistema.
+  Incluye ID, nombre y la lista de miembros.
+  """
+
+  # Campos del struct del equipo
   defstruct [:equipo_id, :nombre, :miembros]
 
+  # Crear una nueva instancia de un equipo
   def nuevo(equipo_id, nombre, miembros) do
     %__MODULE__{
       equipo_id: equipo_id,
