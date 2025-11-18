d938696345ac39bf5d1d19845df595e88679d485
miguel londono <miguela.londonog1@uqvirtual.edu.co>
2025-11-18 01:03:46 -0500

    Agregar comentarios descriptivos al módulo Domain.Proyecto

--- lib/app/domain/proyecto.ex
+++ lib/app/domain/proyecto.ex
@@ -1,8 +1,10 @@
 defmodule HackathonApp.Domain.Proyecto do
   @moduledoc "Entidad Proyecto: representa un proyecto dentro del sistema"
 
+  # Estructura del proyecto con sus campos principales
   defstruct [:id, :equipo_id, :titulo, :descripcion, :categoria, :estado]
 
+  # Constructor para crear un nuevo proyecto
   def nuevo(id, equipo_id, titulo, descripcion, categoria, estado) do
     %__MODULE__{
       id: id,
